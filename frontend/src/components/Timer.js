972e3a9f3de63ab10d6d87d2a0aa99ef586cd71b
Martin Gul <mgul@mm.st>
2021-07-04 20:31:20 -0700

    improve timer component

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -3,39 +3,49 @@ import Utils from '../Utils';
 
 export default class Timer {
     constructor(vnode) {
-        this.end = new Date(new Date() + 1);
+        this.end = new Date(vnode.attrs.end);
         this.remaining = Utils.remaining_time(this.end);
+        this.interval = null;
     }
 
     oncreate(vnode) {
-        this.interval = setInterval(() => {
-            this.remaining = Utils.remaining_time(this.end);
-            console.log(this.remaining);
-            m.redraw();
-        }, 1000);
+        if (this.remaining.days == 0) {
+            this.interval = setInterval(() => {
+                this.remaining = Utils.remaining_time(this.end);
+                m.redraw();
+            }, 1000);
+        }
     }
 
     get_timer_text() {
         let str = '';
         if (this.remaining.days > 0) {
             str += `${this.remaining.days}d `
+        } else {
+            if (this.remaining.hours > 0) {
+                str += `${this.remaining.hours}h `
+            }
+            if (this.remaining.minutes > 0) {
+                str += `${this.remaining.minutes}m `
+            }
+            str += `${this.remaining.seconds}s `;
         }
-        if (this.remaining.hours > 0) {
-            str += `${this.remaining.hours}h `
-        }
-        if (this.remaining.minutes > 0) {
-            str += `${this.remaining.minutes}m `
-        }
-        if (this.remaining.seconds > 0) {
-            str += `${this.remaining.seconds}s `
-        }
+
         if (this.remaining.total < 1000) {
-            clearInterval(this.interval);
+            if (this.interval) {
+                clearInterval(this.interval);
+            }
             str += 'Time is up!';
         } else {
-            str += 'left';
+            str += `left`;
         }
         return str;
+    }
+
+    onremove(vnode) {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
     }
 
     view(vnode) {
