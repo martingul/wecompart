c061126a8927079d43a6b9ad4f80035270317519
Martin Gul <mgul@mm.st>
2021-07-12 11:30:27 -0700

    upgrade packages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -27,35 +27,8 @@ module.exports = {
                 'sans-serif'
             ]
         },
-        extend: {
-            screens: {
-                'xs': '320px',
-            },
-            margin: {
-                '72': '18rem',
-                '80': '20rem',
-                '88': '22rem',
-                '96': '24rem',
-                '104': '26rem',
-            },
-            colors: {
-                'accent': {
-                    '100': '#b3a4d7',
-                    '200': '#a390ce',
-                    '300': '#9a86ca',
-                    '400': '#8f78c4',
-                    '500': '#846bbe',
-                    '600': '#8167bd',
-                    '700': '#6d50b2',
-                    '800': '#61469f',
-                    '900': '#5b4398'
-                },
-            },
-        },
+        extend: {},
     },
-    variants: {
-        backgroundColor: ['responsive', 'hover', 'focus', 'checked', 'disabled'],
-        opacity: ['disabled']
-    },
+    variants: {},
     plugins: [],
 }
