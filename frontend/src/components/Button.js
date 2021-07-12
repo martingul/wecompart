febcd65ed0d1943733f752a646e7f50c1172de51
Martin Gul <mgul@mm.st>
2021-07-11 23:04:21 -0700

    make button more compact and add loading status

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,36 +1,28 @@
 import m from 'mithril';
 import Icon from './Icon';
+import Loading from './Loading';
 
 export default class Button {
     constructor(vnode) {
         this.callback = vnode.attrs.callback ? vnode.attrs.callback : () => {};
         this.icon = vnode.attrs.icon ? vnode.attrs.icon : null;
         this.active = vnode.attrs.active !== undefined ? vnode.attrs.active : true;
+        this.loading = vnode.attrs.loading ? vnode.attrs.loading : null;
     }
 
     view(vnode) {
-        if (!this.icon) {
-            return (
-                <button class={`flex items-center justify-center py-1 px-2 rounded whitespace-nowrap font-bold hover:shadow transition-all
-                + ${this.active ? 'text-white bg-indigo-500 hover:bg-indigo-600'
-                                : 'border border-gray-300 hover:border-gray-300 text-gray-800 bg-white hover:text-black'}`}
-                    onclick={this.callback}>
-                    <span>
+        return (
+            <button type="button" class={`h-9 flex items-center justify-center py-1 px-2 rounded whitespace-nowrap font-bold hover:shadow transition-all
+            + ${this.active ? 'text-white bg-indigo-500 hover:bg-indigo-600'
+                            : 'border border-gray-300 hover:border-gray-300 text-gray-800 bg-white hover:text-black'}`}
+                onclick={this.callback}>
+                <div class="flex items-center">
+                    {(this.icon && ((this.loading && !this.loading()) || !this.loading)) ? <Icon name={this.icon} class="w-5 mr-2" /> : ''}
+                    {(this.loading && this.loading()) ? <Loading color="light" class="w-7" /> : ''}
+                    <span class="flex items-center">
                         {vnode.children}
                     </span>
-                </button>
-            );
-        }
-        
-        return (
-            <button class={`flex items-center justify-center py-1 px-2 rounded whitespace-nowrap font-bold hover:shadow transition-all
-            + ${this.active ? 'text-white bg-indigo-500 hover:bg-indigo-600'
-                            : 'border border-gray-300 hover:border-gray-400 text-gray-800 bg-white hover:text-black'}`}
-                onclick={this.callback}>
-                <Icon name={this.icon} class="w-5" />
-                <span class="ml-2">
-                    {vnode.children}
-                </span>
+                </div>
             </button>
         );
     }
