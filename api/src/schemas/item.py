22b827a8b8566f9cc2c3a7ef081514640227e416
Martin Gul <mgul@mm.st>
2021-07-15 23:58:02 -0700

    change item dimension unit to enum

--- api/src/schemas/item.py
+++ api/src/schemas/item.py
@@ -1,6 +1,11 @@
+from enum import Enum
 from typing import Optional
 from datetime import datetime
 from pydantic import BaseModel, validator
+
+class ItemDimensionUnit(str, Enum):
+    centimeters = 'cm'
+    inches = 'in'
 
 class ItemRead(BaseModel):
     uuid: str
@@ -9,7 +14,7 @@ class ItemRead(BaseModel):
 
     description: str
     quantity: int
-    dim_unit: str
+    dim_unit: ItemDimensionUnit
     length: float
     width: float
     height: float
@@ -24,34 +29,20 @@ class ItemRead(BaseModel):
 class ItemCreate(BaseModel):
     description: str
     quantity: int = 1
-    dim_unit: str
+    dim_unit: ItemDimensionUnit
     length: float
     width: float
     height: float
     weight: float
 
-    @validator('dim_unit')
-    def validate_dim_unit(cls, v):
-        valid_dim_units = ['cm', 'in']
-        if v not in valid_dim_units:
-            raise ValueError('error_invalid_item_dim_unit')
-        return v
-
 class ItemUpdate(BaseModel):
     description: Optional[str] = None
     quantity: Optional[int] = None
-    dim_unit: Optional[str] = None
+    dim_unit: Optional[ItemDimensionUnit] = None
     length: Optional[float] = None
     width: Optional[float] = None
     height: Optional[float] = None
     weight: Optional[float] = None
-
-    @validator('dim_unit')
-    def validate_dim_unit(cls, v):
-        valid_dim_units = ['cm', 'in']
-        if v not in valid_dim_units:
-            raise ValueError('error_invalid_item_dim_unit')
-        return v
 
     @validator('quantity')
     def validate_quantity(cls, v):
